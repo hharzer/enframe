448989350b4ac9da98917bad9a647404c8973c32
Austin Gatlin <austin@gatlin.io>
2019-08-16 19:10:49 +0800

    Complete initial setup.

--- enframe.js
+++ enframe.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
 
-console.log("hey");
-console.log('__dirname: ', __dirname)
+const fs = require('fs')
+const path = require('path')
+
+const rootDir = file => path.join(process.cwd(), file)
+const enframeDir = file => path.join(path.join(process.cwd(), 'node_modules/enframe'), file)
+
+const filesToCopy = []
+filesToCopy.forEach(file => {
+  fs.copyFileSync(enframeDir(file), rootDir(file))
+})
