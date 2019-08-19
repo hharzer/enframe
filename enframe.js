36e3b34a2a1dc30af1432cbf87f722213cfa275a
Austin Gatlin <austin@gatlin.io>
2019-08-19 20:30:16 +0800

    Add some default files.

--- enframe.js
+++ enframe.js
@@ -6,7 +6,7 @@ const path = require('path')
 const rootDir = file => path.join(process.cwd(), file)
 const enframeDir = file => path.join(path.join(process.cwd(), 'node_modules/enframe'), file)
 
-const filesToCopy = []
+const filesToCopy = ['.gitignore', 'LICENSE', 'CODE_OF_CONDUCT.md']
 filesToCopy.forEach(file => {
   fs.copyFileSync(enframeDir(file), rootDir(file))
 })
