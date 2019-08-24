f27beca9f74b70f6ee36e577caa3620d24cd550d
Austin Gatlin <austin@gatlin.io>
2019-08-24 16:27:49 +0700

    Add scripts for test app.

---
+++ test-app-reset.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+pushd enframe-test-app
+git reset --hard v3
+git clean -fd
+git push -f
+
+heroku apps:destroy enframe-test-app-staging --confirm enframe-test-app-staging
+heroku apps:destroy enframe-test-app-prod --confirm enframe-test-app-prod
+popd
